489d80317a313674db8ce993567a8abd5d351bf5
Vivek Verma <vverma240420@gmail.com>
2023-01-25 22:04:23 +0530

    Check for Option

--- SnakeAndLadder/SnakeLadder.cs
+++ SnakeAndLadder/SnakeLadder.cs
@@ -20,6 +20,28 @@ namespace SnakeAndLadder
             Console.WriteLine("Die O/P is:" +roll);
 
 
+
+            //It's check for position
+            int playerPosition = 0;
+            int ladder = 1;
+            int snake = 2;
+            Console.WriteLine("1:ladder\t 2: snake\t 3: notplay");
+            int option = random.Next(1, 4);
+            Console.WriteLine("Choosed option is: " + option);
+            if (option == ladder)
+            {
+                playerPosition = playerPosition + roll;
+                Console.WriteLine("With ladder player moves {0} steps ahead and now on {1} position ", roll, playerPosition);
+            }
+            else if (option == snake)
+            {
+                playerPosition = playerPosition - roll;
+                Console.WriteLine("With snake player moves {0} steps back and now is on {1} position ", roll, playerPosition);
+            }
+            else
+            {
+                Console.WriteLine("no play player stays on its position only no movement " + playerPosition);
+            }
         }
     }
 }
