d3bc66a94e7454341aaa23ae1733054c4a17d55d
Vivek Verma <vverma240420@gmail.com>
2023-01-25 21:53:09 +0530

    RollsTheDie

--- SnakeAndLadder/SnakeLadder.cs
+++ SnakeAndLadder/SnakeLadder.cs
@@ -10,8 +10,15 @@ namespace SnakeAndLadder
     {
         public void Game()
         {
+            //this is for first statement
             int start = 0;
             Console.WriteLine("Start Position:" + start);
+
+            //Rolls the die
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+            Console.WriteLine("Die O/P is:" +roll);
+
 
         }
     }
